c45fa8d9720afb62bb8e61c81bb766927329d316
Elisa <44648415+ElisaA12@users.noreply.github.com>
2023-10-20 19:40:18 +0200

    Add files via upload

---
+++ db.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const SECRET = 'asklkdjwqportityakmajdejnekn';
+ 
+// Initialize database connections
+const db_rent = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'data_rent',
+    //password: '1324',
+    password: 'giulisa',
+    port: 5432,
+});
+  
+const db_user = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'user_web',
+    //password: '1324',
+    password: 'giulisa',
+    port: 5432,
+});
+  
+module.exports = { db_rent, db_user, SECRET };
